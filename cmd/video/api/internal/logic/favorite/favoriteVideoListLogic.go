65e0749f57400d181961b0e5da8306fd4088da5e
Skyenought <1808644906@qq.com>
2023-03-26 11:16:56 +0800

    feat: video.favoriteVideoList 完成

--- cmd/video/api/internal/logic/favorite/favoriteVideoListLogic.go
+++ cmd/video/api/internal/logic/favorite/favoriteVideoListLogic.go
@@ -2,10 +2,15 @@ package favorite
 
 import (
 	"context"
-
+	"github.com/Skyenought/tiktokbackend/cmd/user/rpc/userpc"
+	"github.com/Skyenought/tiktokbackend/cmd/video/api/internal/pack"
+	"github.com/Skyenought/tiktokbackend/cmd/video/rpc/videorpc"
+	"github.com/Skyenought/tiktokbackend/pkg/errno"
+	"github.com/Skyenought/tiktokbackend/pkg/jwtx"
+	
 	"github.com/Skyenought/tiktokbackend/cmd/video/api/internal/svc"
 	"github.com/Skyenought/tiktokbackend/cmd/video/api/internal/types"
-
+	
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -24,5 +29,31 @@ func NewFavoriteVideoListLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *FavoriteVideoListLogic) FavoriteVideoList(req *types.FavoriteVideoListReq) (resp *types.FavoriteVideoListResp, err error) {
-	return
+	var videoList []types.Video
+	userID, err := jwtx.GetUserId(req.Token, l.svcCtx.Config.Auth.Secret)
+	if err != nil {
+		return nil, errno.ErrorHandle(errno.AuthErrCode, "token解析失败: %s", err.Error())
+	}
+	listResp, err := l.svcCtx.VideoRpc.FavoriteVideoList(l.ctx, &videorpc.FavoriteVideoListReq{
+		UserID: req.UserID,
+	})
+	if err != nil {
+		return nil, errno.ErrorHandle(errno.AuthErrCode, "获取收藏视频列表失败: %s", err.Error())
+	}
+	for _, video := range listResp.VideoList {
+		infoResp, err := l.svcCtx.UserRpc.UserInfo(l.ctx, &userpc.UserInfoReq{
+			UserID:     video.UserID,
+			FromUserID: userID,
+		})
+		if err != nil {
+			return nil, errno.ErrorHandle(errno.AuthErrCode, "获取用户信息失败: %s", err.Error())
+		}
+		videoConvHTTP := pack.VideoConvHTTP(video, infoResp.User)
+		videoList = append(videoList, videoConvHTTP)
+	}
+	return &types.FavoriteVideoListResp{
+		StatusCode: errno.SuccessCode,
+		StatusMsg:  "获取收藏视频列表成功",
+		VideoList:  videoList,
+	}, nil
 }
